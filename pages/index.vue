0ba2a132cb12583393b7f594673c4af9ec30a2d8
ImMyst <titouan.galvani@laposte.net>
2018-08-26 23:40:39 +0200

    Add home page content

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,26 @@
 <template>
+  <section id="home">
+    <h1 class="lg-heading animated fadeInUp">Titouan <span class="text-secondary">Galvani</span></h1>
+    <h2 class="sm-heading animated fadeInUp">Développeur web
+      <br>
+      Étudiant en 3<sup>ème</sup>année à Ynov Lyon
+    </h2>
+    <div class="icons animated fadeInUp">
+      <a href="https://www.linkedin.com/in/titouan-galvani/" target="_blank">
+          <i class="fab fa-linkedin fa-2x"></i>
+      </a>
+      <a href="https://github.com/ImMyst" target="_blank">
+          <i class="fab fa-github fa-2x"></i>
+      </a>
+    </div>
+    <footer id="home-footer">
+      <span>Fait avec
+          <i class="fas fa-heart"></i> par Titouan GALVANI ─
+          <a href="https://github.com/ImMyst/Portfolio" class="text-secondary" target="_blank">Code</a>
+      </span>
+    </footer>
+
+  </section>
 </template>
 
 <script>
