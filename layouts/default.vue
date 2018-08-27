2ef81350c244995c14c9a268d364b36021140aff
ImMyst <titouan.galvani@laposte.net>
2018-08-27 18:58:03 +0200

    Add basic styling for elements

--- layouts/default.vue
+++ layouts/default.vue
@@ -18,5 +18,44 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
+* {
+  box-sizing: border-box;
+  &::selection {
+    background-color: #a4ddbb;
+  }
+}
+
+body {
+  margin: 0;
+  padding: 0;
+  background-color: #444;
+  color: #fff;
+  line-height: 1.5;
+  height: 100%;
+  font-family: 'Open Sans',sans-serif;
+  section {
+    padding: 4rem;
+  }
+}
+
+.lg-heading {
+  font-size: 4.5rem;
+  margin: 0;
+}
+
+.sm-heading {
+  margin-bottom: 2rem;
+  margin: 0;
+  padding: .2rem 1rem;
+  background: #494949;
+}
+
+.text-secondary {
+  color: #47bb76;
+}
+
+a {
+  color: #fff;
+}
 </style>
