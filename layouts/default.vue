89f730611667dc0545cc532d104b0a6f9b50298a
ImMyst <titouan.galvani@laposte.net>
2018-08-27 15:47:21 +0200

    Add Footer components

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,17 +2,21 @@
   <div>
     <Header />
     <nuxt/>
+    <Footer />
   </div>
 </template>
 
 <script>
 import Header from "@/components/Header/Header";
+import Footer from "@/components/Footer/Footer";
 
 export default {
   components: {
-    Header: Header
+    Header: Header,
+    Footer: Footer
   }
 };
 </script>
+
 <style>
 </style>
