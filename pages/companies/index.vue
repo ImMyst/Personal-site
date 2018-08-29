c53bc6eade7246615ddd92cd2374bc040ba47943
ImMyst <titouan.galvani@laposte.net>
2018-08-29 23:17:34 +0200

    Add basic headings and styling on companies page

---
+++ pages/companies/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <section id="companies">
+    <h1 class="lg-heading">Les <span class="text-secondary">Entreprises</span></h1>
+    <h2 class="sm-heading">Les entreprises dans lesquelles j'ai travaillé</h2>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+#companies {
+  padding: 6rem 4rem 4rem 4rem;
+  min-height: calc(100vh - 60px);
+}
+</style>
+
