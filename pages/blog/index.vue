69cb2ecb7a38521209f3ff9fd11a6ed85f0fbadd
ImMyst <titouan.galvani@laposte.net>
2018-08-29 23:14:10 +0200

    Add basic headings and styling on blog page

---
+++ pages/blog/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <section id="blog">
+    <h1 class="lg-heading">Mes <span class="text-secondary">Articles</span></h1>
+    <h2 class="sm-heading">Mon ressenti sur mes expériences</h2>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+#blog {
+  padding: 6rem 4rem 4rem 4rem;
+  min-height: calc(100vh - 60px);
+}
+</style>
+
