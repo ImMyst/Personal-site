dcba6429f2d307d453e94ced3c11dbe9a675a91d
ImMyst <titouan.galvani@laposte.net>
2018-08-30 12:59:14 +0200

    Change title

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'Titouan Galvani ─ Accueil',
+    title: 'Titouan Galvani - Accueil',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
