26e0ec683a098a36fab2c7a83b0dd71b762e1c28
ImMyst <titouan.galvani@laposte.net>
2019-10-13 02:13:11 +0200

    🔥 Finish config & add work in progress page

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@
  ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
  */
 module.exports = {
-  theme: {},
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Quicksand']
+      },
+      colors: {
+        'primary-blue': '#355C7D',
+        'secondary-red': '#F67280',
+        'accent-purple': '#6C5B7B',
+        'lighter-red': '#C06C85'
+      }
+    }
+  },
   variants: {},
   plugins: []
 }
