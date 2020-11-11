e2e06223c58cef1b246f1e318e391ff21e7d1e87
ImMyst <titouan.galvani@laposte.net>
2020-11-11 16:40:02 +0100

    🔥 FULL REMOVE

--- README.md
+++
@@ -1,8 +0,0 @@
-# Personal-site
-
-> Refonte complète de mon site internet avec NuxtJS :fire:
-
-
-Made with :heart:  by :
-
-> ImMyst / Titouan GALVANI :metal:
--- pages/projects/index.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="h-full flex justify-center items-center text-center mx-auto">
-    <div class="block container">
-      PROJECTS PAGE
-    </div>
-  </div>
-</template>
