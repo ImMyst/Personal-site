f3b038c57f3dbb90ae7bff76b49f77d93ebf3a94
ImMyst <titouan.galvani@laposte.net>
2020-12-07 20:32:20 +0100

    🔧 More configuration stuff
    Setting up all SVG icons + .github + stylelint.config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,11 +1,5 @@
 export default {
-  // Disable server-side rendering (https://go.nuxtjs.dev/ssr-mode)
-  ssr: false,
-
-  // Target (https://go.nuxtjs.dev/config-target)
   target: 'static',
-
-  // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     title: 'Titouan Galvani - Work In Progress',
     meta: [
@@ -15,27 +9,14 @@ export default {
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
-
-  // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [],
-
-  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [],
-
-  // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
-
-  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
   buildModules: [
-    // https://go.nuxtjs.dev/eslint
     '@nuxtjs/eslint-module',
-    // https://go.nuxtjs.dev/tailwindcss
+    '@nuxtjs/stylelint-module',
     '@nuxtjs/tailwindcss',
   ],
-
-  // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [],
-
-  // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {},
 };
